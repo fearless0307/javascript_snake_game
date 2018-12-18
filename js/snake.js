7adf28d16e59c92b8da73399f942951aa8f6a6ef
Bhola Kumar <bhola.kumar@mountblue.io>
2018-12-18 09:21:22 +0530

    Some changes

--- js/snake.js
+++ js/snake.js
@@ -64,14 +64,14 @@ down.src = "audio/down.mp3";
 let snake = [];
 
 snake[0] = {
-    x : 9 * box,
-    y : 10 * box
+  x : 9 * box,
+  y : 10 * box
 };
 
 // create the food
 let food = {
-    x : Math.floor(Math.random()*17+1) * box,
-    y : Math.floor(Math.random()*15+3) * box
+  x : Math.floor(Math.random()*17+1) * box,
+  y : Math.floor(Math.random()*15+3) * box
 }
 
 // Create wall
@@ -96,197 +96,195 @@ let d;
 document.addEventListener("keydown",direction);
 
 function direction(event){
-    let key = event.keyCode;
-    if( key == 37 && d != "RIGHT"){
-        left.play();
-        d = "LEFT";
-    }else if(key == 38 && d != "DOWN"){
-        d = "UP";
-        up.play();
-    }else if(key == 39 && d != "LEFT"){
-        d = "RIGHT";
-        right.play();
-    }else if(key == 40 && d != "UP"){
-        d = "DOWN";
-        down.play();
-    }else if(key == 32){
-        console.log("Space press");
-
-    }
+  let key = event.keyCode;
+  if( key == 37 && d != "RIGHT"){
+    left.play();
+    d = "LEFT";
+  }else if(key == 38 && d != "DOWN"){
+    d = "UP";
+    up.play();
+  }else if(key == 39 && d != "LEFT"){
+    d = "RIGHT";
+    right.play();
+  }else if(key == 40 && d != "UP"){
+    d = "DOWN";
+    down.play();
+  }else if(key == 32){
+    console.log("Space press");
+  }
 }
 
 // Store scores
 function storeScore() {
-    console.log("Storing Score");
-    var storage = localStorage.getItem('Score'); // Get local storage
-    if(storage) storage = JSON.parse(storage);  // If it exist parse it
-    else storage = [];                          // Otherwise define a empty array
+  console.log("Storing Score");
+  var storage = localStorage.getItem('Score');
+  if(storage) storage = JSON.parse(storage);
+  else storage = [];
 
-    console.log("Before:");
-    console.log(storage);
+  console.log("Before:");
+  console.log(storage);
 
-    // Push value to array
-    storage.push(score);
+  // Push value to array
+  storage.push(score);
 
-    console.log("After:");
-    console.log(storage);
-    // Stringify the array into text to store.
-    localStorage.setItem('Score',JSON.stringify(storage));
+  console.log("After:");
+  console.log(storage);
+  // Stringify the array into text to store.
+  localStorage.setItem('Score',JSON.stringify(storage));
 }
 
 // Store high scores
 function storeHighScore() {
-    console.log("Storing High Score");
+  console.log("Storing High Score");
 
-    console.log("HighScore:");
-    console.log(highScore);
-    // Stringify the array into text to store.
-    localStorage.setItem('HighScore',highScore);
+  console.log("HighScore:");
+  console.log(highScore);
+  // Stringify the array into text to store.
+  localStorage.setItem('HighScore',highScore);
 }
 
 // cheack collision function
 function collision(head,array){
-    for(let i = 0; i < array.length; i++){
-        if(head.x == array[i].x && head.y == array[i].y) return true;
-    }
-    return false;
+  for(let i = 0; i < array.length; i++){
+    if(head.x == array[i].x && head.y == array[i].y) return true;
+  }
+  return false;
 }
 
 // cheack collision wall function
 function collisionWall(head){
-
-    for( let i = 0; i < wallNum.length; i++){
-        let temp = wallNum[i];
-        if(head.x == wall[temp][0].x && head.y == wall[temp][0].y) return true;
-        if(head.x == wall[temp][1].x && head.y == wall[temp][1].y) return true;
-        if(head.x == wall[temp][2].x && head.y == wall[temp][2].y) return true;
-    }
-    return false;
+  for( let i = 0; i < wallNum.length; i++){
+    let temp = wallNum[i];
+    if(head.x == wall[temp][0].x && head.y == wall[temp][0].y) return true;
+    if(head.x == wall[temp][1].x && head.y == wall[temp][1].y) return true;
+    if(head.x == wall[temp][2].x && head.y == wall[temp][2].y) return true;
+  }
+  return false;
 }
 
+// Check apple location
 function appleLocation(){
   console.log("Apple location checking!");
   for( let i = 0; i < wallNum.length; i++){
-      let temp = wallNum[i];
-      if(food.x == wall[temp][0].x && food.y == wall[temp][0].y) return true;
-      if(food.x == wall[temp][1].x && food.y == wall[temp][1].y) return true;
-      if(food.x == wall[temp][2].x && food.y == wall[temp][2].y) return true;
+    let temp = wallNum[i];
+    if(food.x == wall[temp][0].x && food.y == wall[temp][0].y) return true;
+    if(food.x == wall[temp][1].x && food.y == wall[temp][1].y) return true;
+    if(food.x == wall[temp][2].x && food.y == wall[temp][2].y) return true;
   }
   return false;
 }
 
 // draw everything to the canvas
-
 function draw(){
 
-    // Background Color
+  // Background Color
+  ctx.beginPath();
+  ctx.fillStyle = color[colorMode].b;
+  ctx.fillRect(0, 0, 19*box, 19*box);
+  ctx.closePath();
+  ctx.fill();
+
+  // Sub Background Color
+  ctx.beginPath();
+  ctx.fillStyle = color[colorMode].sb;
+  ctx.fillRect(box, 3*box, 17*box, 15*box);
+  ctx.closePath();
+  ctx.fill();
+
+  // Draw color
+  for( let i = 0; i < snake.length ; i++){
     ctx.beginPath();
-    ctx.fillStyle = color[colorMode].b;
-    ctx.fillRect(0, 0, 19*box, 19*box);
+    ctx.fillStyle = ( i==0 )? color[colorMode].snakeH : color[colorMode].snakeB;
+    ctx.fillRect(snake[i].x,snake[i].y,box,box);
+
+    ctx.strokeStyle = color[colorMode].snakeS;
+    ctx.strokeRect(snake[i].x,snake[i].y,box,box);
     ctx.closePath();
-    ctx.fill();
+  }
 
-    // Sub Background Color
-    ctx.beginPath();
-    ctx.fillStyle = color[colorMode].sb;
-    ctx.fillRect(box, 3*box, 17*box, 15*box);
+  // Food color
+  ctx.beginPath();
+  ctx.fillStyle = color[colorMode].appleC;
+  ctx.fillRect(food.x, food.y, box, box);
+  ctx.closePath();
+  ctx.fill();
+
+  // Draw wall
+  for( let i = 0; i < wallNum.length; i++){
+    let temp = wallNum[i];
+    ctx.fillStyle = color[colorMode].wallC;
+    ctx.fillRect(wall[temp][0].x, wall[temp][0].y, box, box);
+    ctx.fillRect(wall[temp][1].x, wall[temp][1].y, box, box);
+    ctx.fillRect(wall[temp][2].x, wall[temp][2].y, box, box);
+    ctx.fill();
     ctx.closePath();
-    ctx.fill();
+  }
 
-    // Draw color
-    for( let i = 0; i < snake.length ; i++){
-        ctx.beginPath();
-        ctx.fillStyle = ( i == 0 )? color[colorMode].snakeH : color[colorMode].snakeB;
-        ctx.fillRect(snake[i].x,snake[i].y,box,box);
+  // old head position
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
 
-        ctx.strokeStyle = color[colorMode].snakeS;
-        ctx.strokeRect(snake[i].x,snake[i].y,box,box);
-        ctx.closePath();
+  // which direction
+  if( d == "LEFT") snakeX -= box;
+  if( d == "UP") snakeY -= box;
+  if( d == "RIGHT") snakeX += box;
+  if( d == "DOWN") snakeY += box;
+
+  // if the snake eats the food
+  if(snakeX == food.x && snakeY == food.y){
+    score++;
+    eat.play();
+    food = {
+      x : Math.floor(Math.random()*17+1) * box,
+      y : Math.floor(Math.random()*15+3) * box
     }
-
-    // Food color
-    ctx.beginPath();
-    ctx.fillStyle = color[colorMode].appleC;
-    ctx.fillRect(food.x, food.y, box, box);
-    ctx.closePath();
-    ctx.fill();
-
-    // Draw wall
-    for( let i = 0; i < wallNum.length; i++){
-        let temp = wallNum[i];
-
-        ctx.fillStyle = color[colorMode].wallC;
-        ctx.fillRect(wall[temp][0].x, wall[temp][0].y, box, box);
-        ctx.fillRect(wall[temp][1].x, wall[temp][1].y, box, box);
-        ctx.fillRect(wall[temp][2].x, wall[temp][2].y, box, box);
-        ctx.fill();
-        ctx.closePath();
-    }
-
-    // old head position
-    let snakeX = snake[0].x;
-    let snakeY = snake[0].y;
-
-    // which direction
-    if( d == "LEFT") snakeX -= box;
-    if( d == "UP") snakeY -= box;
-    if( d == "RIGHT") snakeX += box;
-    if( d == "DOWN") snakeY += box;
-
-    // if the snake eats the food
-    if(snakeX == food.x && snakeY == food.y){
-        score++;
-        eat.play();
+    // Apple location
+    while(true){
+      if(appleLocation()){
         food = {
-            x : Math.floor(Math.random()*17+1) * box,
-            y : Math.floor(Math.random()*15+3) * box
+          x : Math.floor(Math.random()*17+1) * box,
+          y : Math.floor(Math.random()*15+3) * box
         }
-        // Apple location
-        while(true){
-          if(appleLocation()){
-            food = {
-                x : Math.floor(Math.random()*17+1) * box,
-                y : Math.floor(Math.random()*15+3) * box
-            }
-          }else{
-            break;
-          }
-        }
-        // we don't remove the tail
-    }else{
-        // remove the tail
-        snake.pop();
+      }else{
+        break;
+      }
     }
+    // we don't remove the tail
+  }else{
+    // remove the tail
+    snake.pop();
+  }
 
-    // add new Head
-    let newHead = {
-        x : snakeX,
-        y : snakeY
-    }
+  // add new Head
+  let newHead = {
+    x : snakeX,
+    y : snakeY
+  }
 
-    // game over
-    if(snakeX < box || snakeX > 17 * box || snakeY < 3*box || snakeY > 17*box || collision(newHead, snake) || collisionWall(newHead)){
-        clearInterval(game);
-        dead.play();
-        reset();
-    }else{
-      snake.unshift(newHead);
-    }
+  // game over
+  if(snakeX < box || snakeX > 17 * box || snakeY < 3*box || snakeY > 17*box ||
+     collision(newHead, snake) || collisionWall(newHead)){
+    clearInterval(game);
+    dead.play();
+    reset();
+  }else{
+    snake.unshift(newHead);
+  }
 
-    // Showing score
-    ctx.beginPath();
-    ctx.fillStyle = color[colorMode].textC;
-    ctx.font = "45px Changa one";
-    ctx.fillText("Score: " + score, box, 1.6*box);
-    ctx.closePath();
+  // Showing score
+  ctx.beginPath();
+  ctx.fillStyle = color[colorMode].textC;
+  ctx.font = "45px Changa one";
+  ctx.fillText("Score: " + score, box, 1.6*box);
+  ctx.closePath();
 }
 
 // Reset function
 function reset(){
   snake = [];
   snake[0] = {
-      x : 9 * box,
-      y : 10 * box
+    x : 9 * box,
+    y : 10 * box
   };
 
   d = "";
